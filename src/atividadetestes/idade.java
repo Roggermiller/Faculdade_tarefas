38a4e5478afe6d6db30d5254d4adb69c108afc1b
Roggermiller <roge.miller.19@gmail.com>
2023-02-20 00:04:17 -0400

    first commit

---
+++ src/atividadetestes/idade.java
@@ -0,0 +1,25 @@
+package atividadetestes;
+
+public class idade {
+
+	public static void main(String[] args) {
+		int anoAtual;
+		int anoNascimento;
+		int idade;
+		
+		anoAtual = 2023;
+		anoNascimento = 1998;
+		
+		
+		idade = anoAtual - anoNascimento;
+		
+		
+		System.out.println(idade);
+		
+		
+		
+		
+
+	}
+
+}
